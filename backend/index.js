4b8a62a7f191ed80fb27f191d508d86270d50836
Merenciou <vitor.merencio.2012@hotmail.com>
2026-07-13 20:53:04 -0300

    Server initialized

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
+require('dotenv').config;
+const SERVER_PORT = process.env.SERVER_PORT || 3000;
+const SERVER_HOST= process.env.SERVER_HOST || 'localhost';
+const DB_APIKEY= process.env.DB_APIKEY;
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-app.listen()
+app.listen(SERVER_PORT, ()=>{
+    console.log(`Servidor rodando na porta: ${SERVER_PORT}`);
+});
